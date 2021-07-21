d50865c74dff5336c865dd49c333daa0f68a05e9
Nicolas-villarraga <nicovillarraga@hotmail.com>
2021-07-21 14:56:34 -0500

    migrate
    ingreso de tablas a la base de datos

---
+++ database/migrations/2021_07_21_170740_create_clinicalhistories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClinicalhistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clinicalhistories', function (Blueprint $table) {
+            $table->id();
+            $table->string('full history',175);
+            $table->string('specialist',75);
+            $table->string('campus',75);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clinicalhistories');
+    }
+}
---
+++ database/migrations/2021_07_21_170758_create_inventories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',75);
+            $table->string('brand',75);
+            $table->string('description',175);
+            $table->string('location',75);
+            $table->dateTime('expiration-date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
